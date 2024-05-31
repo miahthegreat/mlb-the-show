f38a9a685d4a62e1dad193ed91786e22862e6c2b
miahthegreat <jjschmid85@gmail.com>
2024-05-31 00:02:58 -0400

    updated axios.ts with proper production baseURL

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/api', // Point to your local API route
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://mlb-the-show.vercel.app/api' : 'http://localhost:3000/api', // Point to your local API route
   headers: {
     'Content-Type': 'application/json',
   },
